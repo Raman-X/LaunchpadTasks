421749b7d56d9fad3022e8210712d4b6827adf3e
raman <ramanmaharzan123@gmail.com>
2025-09-17 12:42:29 +0545

    products features done

--- task18/src/controllers/productController.ts
+++ task18/src/controllers/productController.ts
@@ -58,3 +58,62 @@ export const searchProducts = async (req: Request, res: Response) => {
     return res.status(500).json({ message: error.message });
   }
 };
+
+export const reviewProduct = async (req: Request, res: Response) => {
+  try {
+    const { user, rating, comment } = req.body;
+    const productId = req.params.id;
+
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ message: "product not found" });
+    }
+
+    product.reviews?.push({ user, rating, comment });
+    await product.save();
+
+    res.status(200).json({ message: "review added successfully" });
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const { name, category, brand, price, stock, features } = req.body;
+    const productId = req.params.id;
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({ message: "product not found" });
+    }
+
+    if (name !== undefined) product.name = name;
+    if (category !== undefined) product.category = category;
+    if (brand !== undefined) product.brand = brand;
+    if (price !== undefined) product.price = price;
+    if (stock !== undefined) product.stock = stock;
+    if (features !== undefined) product.features = features;
+
+    await product.save();
+    res.status(200).json({ message: "product updated successfully" });
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  try {
+    const productId = req.params.id;
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ message: "product not found" });
+    }
+
+    await product.deleteOne();
+    res.status(200).json({ message: "product deleted successfully" });
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
+  }
+};
--- task18/src/routes/ProductRoutes.ts
+++ task18/src/routes/ProductRoutes.ts
@@ -1,10 +1,19 @@
 import express from "express";
-import { createProduct } from "../controllers/productController";
+import {
+  createProduct,
+  deleteProduct,
+  reviewProduct,
+  searchProducts,
+  updateProduct,
+} from "../controllers/productController";
 
 const router = express.Router();
 
 router.post("/", createProduct);
 router.get("/search", searchProducts);
+router.post("/:id/review", reviewProduct);
+router.patch("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 router.get("/", (req, res) => {
   res.send("Hello World! from api/products");
