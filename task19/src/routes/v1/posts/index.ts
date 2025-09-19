1691d7052413a989bef6cf690ca0554b9e4a4516
raman <ramanmaharzan123@gmail.com>
2025-09-19 09:40:46 +0545

    added middle wares to protected routes

--- task19/src/routes/v1/posts/index.ts
+++ task19/src/routes/v1/posts/index.ts
@@ -1,11 +1,32 @@
 import express from "express";
+import { authMiddleware } from "../../../middlewares/authentication";
+import { authorizationMiddleware } from "../../../middlewares/authorization";
 import postsController from "./controller";
+
 const router = express.Router();
 
-router.post("/", postsController.create);
+router.use(authMiddleware);
+
+router.post(
+  "/",
+  authorizationMiddleware("admin", "blogger"),
+  postsController.create
+);
+
 router.get("/", postsController.getAll);
+
 router.get("/:postId", postsController.getById);
-router.patch("/:postId", postsController.update);
-router.delete("/:postId", postsController.delete);
+
+router.patch(
+  "/:postId",
+  authorizationMiddleware("admin"),
+  postsController.update
+);
+
+router.delete(
+  "/:postId",
+  authorizationMiddleware("admin"),
+  postsController.delete
+);
 
 export default router;
